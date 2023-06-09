da92be744345a5ed90a4dc660e2e41e7105abaca
«SimLove» <simonolyuba@yandex.ru>
2023-06-09 20:41:41 +0300

    add task41

---
+++ task41/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] GetMass(int length)
+{
+    int[] massive = new int[length];
+    for(int i=0;i<length;i++)
+    {
+        Console.WriteLine("Введите число: ");
+        massive[i]=Convert.ToInt32(Console.ReadLine());
+    }
+    return massive;
+}
+
+void CountPositive(int[] massive)
+{
+    int count=0;
+    for(int i=0;i<massive.Length;i++)
+        if(massive[i]>0)
+            count++;
+    Console.WriteLine("Чисел больше нуля: "+count);
+}
+
+Console.WriteLine("Введите количество элементов массива: ");
+CountPositive(GetMass(Convert.ToInt32(Console.ReadLine())));
